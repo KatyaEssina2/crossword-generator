3ddb5dd755f6f3c85ffc52e2f470ad91242d6390
KatyaEssina2 <56380670+KatyaEssina2@users.noreply.github.com>
2020-12-27 18:59:44 +0000

    basic grid

--- generator/views.py
+++ generator/views.py
@@ -4,4 +4,36 @@ from django.http import HttpResponse
 
 def index(request):
     # create crossword
-    return render(request, "index.html")
+    crossword = create_crossword()
+    clues = {"across": ["clue", "clue", "clue"], "down": ["clue", "clue", "clue"]}
+    return render(request, "index.html", {"crossword": crossword, "clues": clues})
+
+
+def create_crossword():
+    # generate theme
+    # create 4 long words for theme
+    # place on grid symmetrically
+    # add empty blocks:
+    # symmetrically
+    # words must be 3+ letters
+    # max 78 words in grid
+    # make sure all non empty spaces are somehow continuous
+    # fill all the words
+    crossword = [
+        ["M", "A", "I", "N", "E", "", "I", "T", "S", "", "E", "L", "S", "A", ""],
+        ["O", "L", "M", "O", "S", "", "D", "I", "E", "", "N", "O", "T", "R", "E"],
+        ["J", "O", "H", "N", "Q", "U", "I", "N", "C", "Y", "A", "D", "A", "M", "S"],
+        ["O", "T", "O", "", "", "S", "O", "T", "", "E", "M", "O", "T", "E", "S"],
+        ["", "", "", "S", "P", "E", "C", "", "E", "S", "E", "", "E", "S", "E"],
+        ["Z", "A", "C", "H", "A", "R", "Y", "T", "A", "Y", "L", "O", "R", "", ""],
+        ["A", "T", "R", "A", "P", "", "", "H", "R", "E", "", "N", "O", "O", "K"],
+        ["R", "O", "A", "M", "", "H", "O", "R", "N", "S", "", "M", "O", "R", "E"],
+        ["A", "B", "B", "A", "", "A", "P", "E", "", "", "A", "I", "M", "E", "R"],
+        ["", "", "A", "N", "D", "R", "E", "W", "J", "A", "C", "K", "S", "O", "N"],
+        ["T", "S", "P", "", "O", "D", "D", "", "A", "N", "T", "E", "", "", ""],
+        ["A", "L", "P", "I", "N", "E", "", "A", "B", "A", "", "", "C", "B", "S"],
+        ["M", "I", "L", "L", "A", "R", "D", "F", "I", "L", "L", "M", "O", "R", "E"],
+        ["S", "M", "E", "L", "T", "", "O", "R", "R", "", "E", "E", "R", "I", "E"],
+        ["", "E", "S", "S", "E", "", "S", "O", "U", "", "E", "N", "D", "O", "R"],
+    ]
+    return crossword
